5af5f95d544ad078ef52866aecdd6da18c26abf1
takehiro <uktk.1022.zz123@outlook.jp>
2020-02-27 23:14:24 +0900

    little adjustment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :notes
-  has_many :tasks
+  has_many :notes, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 end
